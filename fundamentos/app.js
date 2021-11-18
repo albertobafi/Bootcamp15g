6e5b0d41890e7f273d7da986a16f870d34b44090
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-11-17 21:32:07 -0600

    js

---
+++ fundamentos/app.js
@@ -0,0 +1,14 @@
+console.log('hola desde js')
+
+/*dasdasfas*/ 
+//forma de poner comentarios
+
+let age = 20
+console.log('edad:', age)
+age = 25
+console.log('edad: ', age)
+
+let firstName
+console.log('nombre:', firstName)
+firstName = 'beto'
+console.log('nombre:', firstName)
