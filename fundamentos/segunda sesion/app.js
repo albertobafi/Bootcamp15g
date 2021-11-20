213d4cc65ce08707362631349de1439c64b14975
José Alberto Barragán Figueroa <josebafi93@outlook.com>
2021-11-19 23:05:36 -0600

    orden de carros

---
+++ fundamentos/segunda sesion/app.js
@@ -0,0 +1,62 @@
+// let age = 15
+// if (age >= 18){
+//     console.log("ya puede votar")
+// }
+// console.log("continua programa")
+
+
+// let cafe = false
+// if (cafe === true){
+//     console.log("hoy tomaré cafe")
+// } else {
+//     console.log("hoy tomaré té")
+// }
+//     console.log("continua programa")
+
+
+// let number = -10
+// if(number === 0){
+//     console.log("es un numero neutral")
+// } else if (number > 0) {
+//     console.log("Es un numero positivo")
+// } else {
+//     console.log("Es un numero negativo")
+// }
+
+
+// let operation = prompt("introduce la operacion a realizar") // suma, resta, division, multiplicacion.
+// let number1 = parseInt(prompt("introduce el numero"))
+// let number2 = parseInt(prompt("introduce el numero"))
+// console.log("operacion", operation)
+// switch (operation){
+//         case "suma":
+//             console.log("La suma es: ", number1 + number2)
+//             break
+//         case "resta":
+//              console.log("la resta es: ", number1 - number2)
+//              break
+//         case "division":
+//              console.log("la division es: ", number1 / number2)
+//              break
+//         case "multiplicacion":
+//              console.log("la multiplicacion es: ", number1 * number2)
+//              break
+//         default:
+//             console.log("operacion no permitida")
+// }
+// console.log("sigue el programa") 
+
+let age = 100
+let wweeks = 800
+if (age >= 60 && wweeks >= 750){
+     console.log("Pensionadisimo")
+} else {
+     console.log("le falta, siga trabajando")
+}
+
+let weather = prompt("introduce tu clima actual: ")
+if (weather == 'lloviendo'){
+    console.log("vea pelis")
+} else {
+    console.log("saque al perro")
+}
